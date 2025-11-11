b262f92bb4c38bfab382befee76c7e412b7c4c77
潜龙勿用 <72674992+soda-code@users.noreply.github.com>
2025-11-11 08:18:43 +0800

    Delete LVGL directory

--- LVGL/STM32F407/Middlewares/LVGL/GUI_APP/lv_curve_Screen.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef __LV_CURET_H
-#define __LV_CURET_H
-
-void CURVE_DISPLAY(void);
-
-#endif
--- LVGL/STM32F407/Middlewares/LVGL/GUI_APP/lv_mainstart.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __LV_MAINSTART_H
-#define __LV_MAINSTART_H
-
-void HOME_DISPLAY(void);
-void LV_Main_Win();
-
-#endif
