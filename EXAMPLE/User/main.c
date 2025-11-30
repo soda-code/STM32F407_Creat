150c2db9b5ac2e49376f856638ab29c70c2722b0
liqingbo <18895391769@163.com>
2025-11-30 22:26:09 +0800

    1:更改格式

--- EXAMPLE/User/main.c
+++ EXAMPLE/User/main.c
@@ -18,7 +18,7 @@ int main(void)
     
     led_init();                         /* 初始化LED */
 	lcd_init();                             /* 初始化LCD */
-	//can_init(CAN_SJW_1TQ, CAN_BS2_6TQ, CAN_BS1_7TQ, 6, CAN_MODE_NORMAL);  /* CAN初始化, 环回模式, 波特率500Kbps */
+	can_init(CAN_SJW_1TQ, CAN_BS2_6TQ, CAN_BS1_7TQ, 6, CAN_MODE_NORMAL);  /* CAN初始化, 环回模式, 波特率500Kbps */
 	my_mem_init(SRAMIN);                /* 初始化内部SRAM内存池 */
     //USB_Init();                        /* 初始化USB */
 	freertos_demo();                    /* 运行FreeRTOS */
